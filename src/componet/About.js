c8990af26b567143b9900fd40b409f28fe60db0d
Hemanshu Savaliya <160478058+HemanshuSavaliya@users.noreply.github.com>
2024-04-17 11:29:01 +0530

    Update About.js

--- src/componet/About.js
+++ src/componet/About.js
@@ -2,8 +2,14 @@ import React from "react";
 import Section from "./Section";
 
 const About = () => {
+  const containerStyle = {
+    backgroundImage: "url('https://cdn.rentechdigital.com/common_files/rentech/common-images/home-hero-shap-left.png')",
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+  };
   return (
-    <div className="container animate__animated animate__fadeIn">
+    <div className="container animate__animated animate__fadeIn" style={containerStyle}>
       <div className="col-xl-9 mx-auto">
         <div className="comn-hero-section-main text-center pt-5 mt-5">
           <h1 className=" fw-bold py-4 " style={{ fontSize: "3rem" }}>
@@ -19,83 +25,92 @@ const About = () => {
           </h6>
         </div>
       </div>
-      <div className="row ">
-        <div className="col-md-4 ">
+      <div className="row">
+        <div className="col-md-4 mb-4">
           <img
             src="https://cdn.rentechdigital.com/common_files/rentech/grid-about-5.png"
-            className="img-fluid"
+            className="img-fluid rounded"
             alt=""
+            style={{maxHeight: "410px", width: "100%" }}
           />
         </div>
-        <div className="col-md-4">
+        <div className="col-md-4 mb-4">
           <img
             src="https://cdn.rentechdigital.com/common_files/rentech/grid-about-1.png"
-            className="img-fluid "
+            className="img-fluid rounded"
             alt=""
+            style={{maxHeight: "410px", width: "100%" }}
           />
         </div>
-        <div className="col-md-4">
+        <div className="col-md-4 ">
           <img
             src="https://media.istockphoto.com/id/185255095/photo/group-of-business-people-having-meeting-around-tablet-computer.jpg?s=612x612&w=0&k=20&c=h9wy4Byt6BFgtcds6u1ny4izNy5f7Lby72OPSK3Ck10="
-            className="img-fluid m-0"
+            className="img-fluid rounded"
             alt=""
-            style={{ height: "410px", width: "400px" }}
+            style={{ maxHeight: "410px", width: "100%" }}
           />
         </div>
         <div className="col-md-4">
           <img
             src="https://cdn.rentechdigital.com/common_files/rentech/grid-about-4.png"
-            className="img-fluid pt-4"
+            className="img-fluid rounded pt-4"
             alt=""
+            style={{ width: "100%" }}
           />
         </div>
         <div className="col-md-4">
           <img
             src="https://cdn.rentechdigital.com/common_files/rentech/grid-about-2.png"
-            className="img-fluid pt-4"
+            className="img-fluid rounded pt-4"
             alt=""
-            style={{ height: "280px", width: "400px" }}
+            style={{ width: "100%" }}
           />
         </div>
         <div className="col-md-4">
           <img
             src="https://cdn.rentechdigital.com/common_files/rentech/grid-about-3.png"
-            className="img-fluid pt-4"
+            className="img-fluid rounded pt-4"
             alt=""
+            style={{ maxHeight: "330px", width: "100%" }}
           />
         </div>
       </div>
+
       <section className="row-inr-space-class">
-        <div className="row align-items-center ">
-          <div className="col-xl-5 col-lg-6">
-            <div className="text-lg-start text-center pb-5 pb-lg-0 position-relative pt-5 mt-5">
-              <img
-                loading="lazy"
-                src="https://cdn.rentechdigital.com/common_files/common/enterprises-image.png"
-                className="img-fluid"
-                alt="solve business problems"
-                style={{ width: "600px", height: "400px" }}
-              />
+        <div className="container">
+          <div className="row align-items-center">
+            <div className="col-xl-6 col-lg-6 order-lg-2">
+              <div className="text-center pb-5 pb-lg-0 pt-5 mt-5">
+                <img
+                  loading="lazy"
+                  src="https://cdn.rentechdigital.com/common_files/common/enterprises-image.png"
+                  className="img-fluid"
+                  alt="solve business problems"
+                  style={{ maxWidth: "100%", height: "auto" }}
+                />
+              </div>
             </div>
-          </div>
-          <div className="col-lg-6 ms-lg-auto">
-            <div className="comn-hero-section-main d-block ">
-              <h2>
-                Helping solve business problems, discover new opportunities
-              </h2>
-              <h6>
-                We assist customers in the most practical, suitable, and
-                cost-effective manner possible, and we are delighted to have
-                solved some of their most challenging problems. Thousands of
-                companies have benefited from our solutions and have faith in
-                our ability to get the necessary results. We continue to
-                cultivate customer connections and are engaged in their success.
-              </h6>
+            <div className="col-xl-6 col-lg-6 ms-lg-auto">
+              <div className="comn-hero-section-main d-block text-lg-start text-center pt-5 mt-5">
+                <h2>
+                  Helping solve business problems, discover new opportunities
+                </h2>
+                <h6>
+                  We assist customers in the most practical, suitable, and
+                  cost-effective manner possible, and we are delighted to have
+                  solved some of their most challenging problems. Thousands of
+                  companies have benefited from our solutions and have faith in
+                  our ability to get the necessary results. We continue to
+                  cultivate customer connections and are engaged in their
+                  success.
+                </h6>
+              </div>
             </div>
           </div>
         </div>
       </section>
-      <div className=" text-center pt-5 my-5 bg-dark text-white">
+
+      <div className="text-center pt-5 my-5 bg-dark text-white">
         <h1>A new method, a new beginning</h1>
         <h6 className="pt-5">
           When you choose our products, you get much more to grow your business.
@@ -105,46 +120,47 @@ const About = () => {
           for today and tomorrow.
         </h6>
         <ul className="row bg-gradt-inr-text-main list-inline pt-5 ">
-          <li className="col-md-4 col-6 p-0">
-            <div className="text-center bg-gradt-inr-text py-4  border-end">
+          <li className="col-lg-4 col-md-6 col-sm-12 p-0 mb-4">
+            <div className="text-center bg-gradt-inr-text py-4">
               <h3>1500+</h3>
               <h5>Customers Served</h5>
             </div>
           </li>
-          <li className="col-md-4 col-6 p-0">
-            <div className="text-center bg-gradt-inr-text py-4 border-end">
+          <li className="col-lg-4 col-md-6 col-sm-12 p-0 mb-4">
+            <div className="text-center bg-gradt-inr-text py-4">
               <h3 className="d-block">90+</h3>
               <h5 className="d-block">Expert Team</h5>
             </div>
           </li>
-          <li className="col-md-4 col-6 p-0">
+          <li className="col-lg-4 col-md-6 col-sm-12 p-0 mb-4">
             <div className="text-center bg-gradt-inr-text py-4">
               <h3 className="d-block">2014</h3>
               <h5 className="d-block">Founded</h5>
             </div>
           </li>
-          <li className="col-md-4 col-6 p-0 pt-5">
-            <div className="text-center bg-gradt-inr-text py-4  border-end">
+          <li className="col-lg-4 col-md-6 col-sm-12 p-0 mb-4">
+            <div className="text-center bg-gradt-inr-text py-4">
               <h3 className="d-block">98%</h3>
               <h5 className="d-block">Customer Satisfaction</h5>
             </div>
           </li>
-          <li className="col-md-4 col-6 p-0 pt-5">
-            <div className="text-center bg-gradt-inr-text py-4  border-end">
+          <li className="col-lg-4 col-md-6 col-sm-12 p-0 mb-4">
+            <div className="text-center bg-gradt-inr-text py-4">
               <h3 className="d-block">155+</h3>
               <h5 className="d-block">Countries Covered</h5>
             </div>
           </li>
-          <li className="col-md-4 col-6 p-0 pt-5 pb-5">
-            <div className="text-center bg-gradt-inr-text py-4  ">
+          <li className="col-lg-4 col-md-6 col-sm-12 p-0 mb-4">
+            <div className="text-center bg-gradt-inr-text py-4">
               <h3 className="d-block">100%</h3>
               <h5 className="d-block">Customer Support</h5>
             </div>
           </li>
         </ul>
       </div>
-      <div className="d-flex">
-        <div className="text-start  py-4 col-lg-6">
+
+      <div className="d-flex flex-wrap justify-content-center">
+        <div className="text-start py-4 col-lg-6 col-md-12 order-2 order-md-1">
           <h2 className="pt-3 fw-bold">
             SAVALIYA INFOTECH’s customer-first approach
           </h2>
@@ -158,7 +174,7 @@ const About = () => {
           </h6>
           <h6>We help you rise above the ordinary to achieve your goal,</h6>
         </div>
-        <div className="card shadow-lg m-5 bg-body rounded text-center mt-5 ">
+        <div className="card shadow-lg m-5 bg-body rounded text-center mt-5 col-lg-4 col-md-6 col-sm-12 order-1 order-md-2">
           <div className="card-body">
             <h1 className="card-title fw-bold" style={{ color: "#eb5a95" }}>
               save people time{" "}
@@ -170,6 +186,7 @@ const About = () => {
           </div>
         </div>
       </div>
+
       <div className=" text-center">
         <div className="card shadow-lg p-3 mb-5 bg-body rounded col-lg-6">
           <div className="card-body">
@@ -177,21 +194,25 @@ const About = () => {
             <h1>customer-centric approach</h1>
           </div>
         </div>
-        <div className="d-flex ">
-          <h1 className="pt-5 mt-4 col-lg-6" style={{ color: "#eb5a95" }}>
+        <div className="d-flex flex-column flex-md-row align-items-start">
+          <h1
+            className="pt-5 mt-4 col-lg-6 col-md-10"
+            style={{ color: "#eb5a95" }}
+          >
             Serve with integrity
           </h1>
-          <div class="comn-hero-section-main d-block text-start pb-5">
+          <div className="comn-hero-section-main d-block text-start pb-5 col-lg-6 col-md-10">
             <h1>Committed to our core values</h1>
             <h6>
               We want our business partnerships to last for a long time so that
               everyone benefits. We are firm believers in intelligent solutions
               that automate and expand our clients' business operations and
               procedures. To meet our client’s different needs, we adhere to the
-              essential principle of "End to End Solutions.
+              essential principle of "End to End Solutions."
             </h6>
           </div>
         </div>
+
         <div className="card shadow-lg p-3 mb-5 bg-body rounded col-lg-6">
           <div className="card-body">
             <h1 className="card-title">
@@ -203,7 +224,7 @@ const About = () => {
       <section class="row-inr-space-class about-box-bg-image bg-dark text-white p-5">
         <div class="container">
           <div class="row align-items-center">
-            <div class="col-lg-4">
+            <div class="col-lg-4 col-md-6">
               <div class="swipe-left-right-image text-lg-start text-center">
                 <img
                   loading="lazy"
@@ -213,13 +234,14 @@ const About = () => {
                 />
               </div>
             </div>
-            <div class="col-lg-8 ps-lg-5">
+            <div class="col-lg-8 col-md-6 ps-lg-5">
               <div class="btm-banner-info">
                 <h2 class="mb-0">
-                  <bdi>“</bdi>SAVALIYA INFOTECH’s Swipecart was all that I needed
-                  to boost my online sales. The conversion from the website to a
-                  world-class mobile app was completed in a few days. I can
-                  assure you that their product is amazing<bdi>”</bdi>
+                  <bdi>“</bdi>SAVALIYA INFOTECH’s Swipecart was all that I
+                  needed to boost my online sales. The conversion from the
+                  website to a world-class mobile app was completed in a few
+                  days. I can assure you that their product is amazing
+                  <bdi>”</bdi>
                 </h2>
                 <div class="pt-4">
                   <img
@@ -234,20 +256,21 @@ const About = () => {
           </div>
         </div>
       </section>
-      <div class="row">
-        <div class="col-xl-9 col-md-8 mx-auto">
+
+      <div class="row justify-content-center">
+        <div class="col-xl-9 col-md-10 col-sm-12 mx-auto">
           <div class="text-center sl-fstr-text py-5">
             <h2>Differentiate Your Brand, Dominate Your Marketplace</h2>
             <h6>
               We have the ideal solutions to meet your specific company
               requirements. Are you ready to do your best work? We're here to
-              help you.{" "}
+              help you.
             </h6>
           </div>
         </div>
       </div>
-      
-      <Section/>
+
+      <Section />
     </div>
   );
 };
