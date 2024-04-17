7d828c89f5059a5128653796ce9d386a637d59f2
Hemanshu Savaliya <160478058+HemanshuSavaliya@users.noreply.github.com>
2024-04-17 11:29:51 +0530

    Update Navbar.js

--- src/componet/Navbar.js
+++ src/componet/Navbar.js
@@ -1,49 +1,46 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-
 const Navbar = () => {
-  
-
   return (
-    <div>
-      <nav className="navbar navbar-expand-lg navbar-expand navbar-dark bg-dark fixed-top ">
-        <div className="container-fluid">
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <Link
-                  className="nav-link active"
-                  aria-current="page"
-                  to="/home"
-                >
-                  Home
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link
-                  className="nav-link active"
-                  aria-current="page"
-                  to="/about"
-                >
-                  About Us
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link
-                  className="nav-link active"
-                  aria-current="page"
-                  to="/contect"
-                >
-                  Contect Us
-                </Link>
-              </li>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <div className="container-fluid">
+        <Link className="navbar-brand" to="/home">
+            SAVALIYA INFOTECH 🤝
+        </Link>
+        <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+        >
+            <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+            <ul className="navbar-nav">
+                <li className="nav-item">
+                    <Link className="nav-link" to="/home">
+                        Home
+                    </Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="nav-link" to="/about">
+                        About Us
+                    </Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="nav-link" to="/contact">
+                        Contact Us
+                    </Link>
+                </li>
             </ul>
-            <h1 className="text-white">SAVALIYA INFOTECH 🤝</h1>
-          </div>
         </div>
-      </nav>
     </div>
+</nav>
+
   );
 };
 
