9c32843b14870e1c3333877f619d67d49e3d3119
Hemanshu Savaliya <160478058+HemanshuSavaliya@users.noreply.github.com>
2024-04-17 11:30:18 +0530

    Update Section.js

--- src/componet/Section.js
+++ src/componet/Section.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from "react";
 
 const Section = () => {
   return (
     <div>
-    <div className="row align-items-center position-relative container py-5">
+      <div className="row align-items-center position-relative container py-5">
         <div className="position-absolute btm-fix-image ">
           <img
             src="https://cdn.rentechdigital.com/common_files/rentech/common-images/btm-box-fix-image.png"
@@ -39,10 +39,11 @@ const Section = () => {
           </div>
         </div>
       </div>
+
       <section className="ftr-space-info bg-dark pt-5">
         <div className="container">
-          <div className="row me-0">
-            <div className="col-lg-3 col-md-4 pe-0 mb-4 mb-lg-0">
+          <div className="row">
+            <div className="col-lg-3 col-md-6 mb-4 mb-lg-0">
               <div className="logo-left-text-ftr">
                 <p className="text-white">
                   SAVALIYA INFOTECH specializes in enterprise solutions for all
@@ -50,47 +51,45 @@ const Section = () => {
                 </p>
               </div>
               <div className="logo-left-text-ctr">
-                <div className="row me-0 align-items-center">
-                  <div className="col-md-12 col-6 pe-0">
-                    <div className="mt-md-0 mt-3">
+                <div className="row align-items-center">
+                  <div className="col-md-6 col-6">
+                    <div className="mt-3">
                       <img
                         src="https://cdn.rentechdigital.com/common_files/rentech/common-images/google-partner-logo2.svg"
-                        className="img-fluid m-0"
+                        className="img-fluid"
                         alt="google"
                       />
                     </div>
                   </div>
-                  <div className="col-md-12 col-6 pe-0">
+                  <div className="col-md-6 col-6">
                     <div className="mt-3">
                       <img
                         src="https://cdn.rentechdigital.com/common_files/rentech/common-images/shopify-logo-white.svg"
                         loading="lazy"
-                        className="img-fluid m-0"
+                        className="img-fluid"
                         alt="shopify partner"
                       />
                     </div>
                   </div>
                 </div>
               </div>
-              <div className="bdr-top-info"></div>
+              <div className="bdr-top-info mt-3"></div>
             </div>
-            <div className="col-lg-6 col-md-8 mx-auto pe-0 mb-4 mb-lg-0">
-              <div className="row me-0">
-                <div className="col-sm-4 col-6 pe-0">
+
+            <div className="col-lg-6 col-md-8 mx-auto mb-4 mb-lg-0">
+              <div className="row">
+                <div className="col-sm-4 col-6 mb-4 mb-sm-0">
                   <div className="footer-links-info">
                     <h4 className="text-white text-center pb-3">Navigation</h4>
-                    <ul>
+                    <ul className="list-unstyled">
                       <li>
-                        <a
-                          className="text-capitalize d-inline-block text-white text-decoration-none "
-                          href="/"
-                        >
+                        <a className="text-white text-decoration-none" href="/">
                           Home
                         </a>
                       </li>
                       <li>
                         <a
-                          className="text-capitalize d-inline-block text-white text-decoration-none"
+                          className="text-white text-decoration-none"
                           href="/about"
                         >
                           About
@@ -98,15 +97,15 @@ const Section = () => {
                       </li>
                       <li>
                         <a
-                          className="text-capitalize d-inline-block text-white text-decoration-none"
+                          className="text-white text-decoration-none"
                           href="/swipecart/features"
                         >
-                          features
+                          Features
                         </a>
                       </li>
                       <li>
                         <a
-                          className="text-capitalize d-inline-block text-white text-decoration-none"
+                          className="text-white text-decoration-none"
                           href="/customer"
                         >
                           Customer
@@ -114,7 +113,7 @@ const Section = () => {
                       </li>
                       <li>
                         <a
-                          className="text-capitalize d-inline-block text-white text-decoration-none"
+                          className="text-white text-decoration-none"
                           href="/blog?page=1"
                         >
                           Blog
@@ -123,24 +122,24 @@ const Section = () => {
                     </ul>
                   </div>
                 </div>
-                <div className="col-sm-4 col-6 pe-0">
+                <div className="col-sm-4 col-6 mb-4 mb-sm-0">
                   <div className="footer-links-info">
                     <h4 className="text-white text-center pb-3">
-                      useful links
+                      Useful Links
                     </h4>
-                    <ul>
+                    <ul className="list-unstyled">
                       <li>
                         <a
+                          className="text-white text-decoration-none"
                           href="https://docs.rentechdigital.com/en/"
                           target="_blank"
-                          className="text-capitalize d-inline-block text-white text-decoration-none"
                         >
                           Help Center
                         </a>
                       </li>
                       <li>
                         <a
-                          className="text-capitalize d-inline-block text-white text-decoration-none"
+                          className="text-white text-decoration-none"
                           href="/contact-us"
                         >
                           Contact Us
@@ -148,42 +147,42 @@ const Section = () => {
                       </li>
                       <li>
                         <a
-                          className="text-capitalize d-inline-block text-white text-decoration-none"
+                          className="text-white text-decoration-none"
                           href="/policy"
                         >
-                          privacy policy
+                          Privacy Policy
                         </a>
                       </li>
                       <li>
                         <a
-                          className="text-capitalize d-inline-block text-white text-decoration-none"
+                          className="text-white text-decoration-none"
                           href="/terms-service"
                         >
-                          terms of use
+                          Terms of Use
                         </a>
                       </li>
                       <li>
                         <a
-                          className="text-capitalize d-inline-block text-white text-decoration-none"
+                          className="text-white text-decoration-none"
                           href="/refunds-cancellations"
                         >
-                          cancel &amp; refund policy
+                          Cancel &amp; Refund Policy
                         </a>
                       </li>
                       <li>
                         <a
-                          className="text-capitalize d-inline-block text-white text-decoration-none"
+                          className="text-white text-decoration-none"
                           href="/career"
                         >
-                          careers{" "}
-                          <bdi className="hiring-btn ms-1">We’re Hiring!</bdi>
+                          Careers{" "}
+                          <b className="hiring-btn ms-1">We’re Hiring!</b>
                         </a>
                       </li>
                       <li>
                         <a
-                          target="_blank"
-                          className="text-capitalize d-inline-block text-white text-decoration-none"
+                          className="text-white text-decoration-none"
                           href="https://calendly.com/rentechdigital/free-enterprise"
+                          target="_blank"
                         >
                           Hire an Expert
                         </a>
@@ -191,13 +190,13 @@ const Section = () => {
                     </ul>
                   </div>
                 </div>
-                <div className="col-sm-4 col-12 pe-0 mt-4 mt-sm-0">
+                <div className="col-sm-4 col-12">
                   <div className="footer-links-info">
                     <h4 className="text-white pb-3">Product</h4>
-                    <ul>
+                    <ul className="list-unstyled">
                       <li>
                         <a
-                          className="text-capitalize d-inline-block text-white  text-decoration-none"
+                          className="text-white text-decoration-none"
                           href="/swipecart"
                         >
                           Swipecart
@@ -205,7 +204,7 @@ const Section = () => {
                       </li>
                       <li>
                         <a
-                          className="text-capitalize d-inline-block text-white text-decoration-none"
+                          className="text-white text-decoration-none"
                           href="/smartscraper"
                         >
                           Smartscrapers
@@ -213,7 +212,7 @@ const Section = () => {
                       </li>
                       <li>
                         <a
-                          className="text-capitalize d-inline-block text-white text-decoration-none"
+                          className="text-white text-decoration-none"
                           href="/dotcom"
                         >
                           dotcom
@@ -224,75 +223,76 @@ const Section = () => {
                 </div>
               </div>
             </div>
-            <div className="col-lg-3 pe-0 mb-4 mb-lg-0">
+
+            <div className="col-lg-3 mb-4 mb-lg-0">
               <div className="footer-links-info">
                 <h4 className="text-white pb-3 text-center">Get in touch</h4>
-                <ul className="ftr-links-flag-info">
-                  <li>
-                    <div className=" d-flex align-items-start pb-2 text-start">
+                <ul className="list-unstyled">
+                  <li className="mb-3">
+                    <div className="d-flex align-items-start">
                       <img
                         src="https://cdn.rentechdigital.com/common_files/rentech/common-images/usa-flag-new.svg"
                         loading="lazy"
-                        className="img-fluid"
+                        className="img-fluid me-2"
                         alt="usa"
                       />
-                      <p className="ps-2 mb-0">
-                        <a
-                          target="_blank"
-                          className="text-capitalize d-inline-block text-white text-decoration-none"
-                          href="https://g.page/rentech-digital"
-                        >
-                          1180 Avenue of the Americas 9th floor, New York, NY
-                          10036
-                        </a>
-                      </p>
+                      <div>
+                        <p className="mb-0">
+                          <a
+                            target="_blank"
+                            className="text-capitalize d-inline-block text-white text-decoration-none"
+                            href="https://g.page/rentech-digital"
+                          >
+                            1180 Avenue of the Americas 9th floor, New York, NY
+                            10036
+                          </a>
+                        </p>
+                      </div>
                     </div>
                   </li>
-                  <li>
-                    <div className=" d-flex align-items-start">
+                  <li className="mb-3">
+                    <div className="d-flex align-items-start">
                       <img
                         src="https://t3.ftcdn.net/jpg/06/12/34/84/360_F_612348476_k3i3brMUOo3N9Wk3yoceMHqZ8w4jpFqu.jpg"
                         loading="lazy"
-                        className="img-fluid"
+                        className="img-fluid me-2"
                         alt="usa"
                         style={{ width: "25px", height: "20px" }}
                       />
-                      <p className="ps-2 mb-0">
-                        <a
-                          target="_blank"
-                          className="text-capitalize d-inline-block text-white text-decoration-none"
-                          href="https://g.page/rentech-digital"
-                        >
-                          401, Amby Valley Arcade, opp. Santosa Heights, Uttran,
-                          Surat, Gujarat 394101
-                        </a>
-                      </p>
+                      <div>
+                        <p className="mb-0">
+                          <a
+                            target="_blank"
+                            className="text-capitalize d-inline-block text-white text-decoration-none"
+                            href="https://g.page/rentech-digital"
+                          >
+                            401, Amby Valley Arcade, opp. Santosa Heights,
+                            Uttran, Surat, Gujarat 394101
+                          </a>
+                        </p>
+                      </div>
+                    </div>
+                  </li>
+                  <li className="mb-3">
+                    <div className="d-flex align-items-center">
+                      <i className="bi bi-telephone-fill text-white me-2"></i>
+                      <a
+                        className="d-inline-block text-white mb-0 text-decoration-none"
+                        href="tel:+17603899794"
+                      >
+                        +91 9104795041
+                      </a>
                     </div>
                   </li>
                   <li>
-                    <div className="ftr-links-btm-info-icon pt-3">
-                      <i className="bi bi-telephone-fill text-white"></i>
-                      <p className="ps-2 d-flex align-items-center">
-                        <a
-                          className="d-inline-block text-white mb-0 text-decoration-none"
-                          href="tel:+17603899794"
-                        >
-                          +91 9104795041
-                        </a>
-                      </p>
-                    </div>
-                  </li>
-                  <li>
-                    <div className="ftr-links-btm-info-icon">
-                      <i className="bi bi-envelope-fill text-white "></i>
-                      <p className="mb-0 ps-2 d-flex align-items-center">
-                        <a
-                          className="d-inline-block text-white mb-0 text-decoration-none"
-                          href="mailto:team@rentechdigital.com"
-                        >
-                          team@savaliyainfotech.com
-                        </a>
-                      </p>
+                    <div className="d-flex align-items-center">
+                      <i className="bi bi-envelope-fill text-white me-2"></i>
+                      <a
+                        className="d-inline-block text-white mb-0 text-decoration-none"
+                        href="mailto:team@rentechdigital.com"
+                      >
+                        team@savaliyainfotech.com
+                      </a>
                     </div>
                   </li>
                 </ul>
@@ -302,7 +302,7 @@ const Section = () => {
         </div>
       </section>
     </div>
-  )
-}
+  );
+};
 
-export default Section
+export default Section;
